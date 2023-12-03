a8f099435ca32edfe1afab6acbe09a73e29489c9
Disk-MTH <gillet.mat@free.fr>
2023-12-03 13:14:11 +0100

    Change things in mcd

--- mcd.sql
+++ mcd.sql
@@ -11,12 +11,20 @@ GO;
 
 CREATE TABLE address
 (
-    id_address  INT IDENTITY (1,1) NOT NULL,
-    street_name VARCHAR(50)        NOT NULL,
-    city        VARCHAR(50)        NOT NULL,
-    zip_code    INT                NOT NULL,
-    deleted     BINARY             NOT NULL DEFAULT 0,
+    id_address INT IDENTITY (1,1) NOT NULL,
+    street     VARCHAR(50)        NOT NULL,
+    city       VARCHAR(50)        NOT NULL,
+    zip_code   INT                NOT NULL,
+    deleted    BIT                NOT NULL DEFAULT 0,
     PRIMARY KEY (id_address)
+);
+GO;
+
+CREATE TABLE address_type
+(
+    id_address_type INT IDENTITY (1,1) NOT NULL,
+    type            VARCHAR(100)       NOT NULL,
+    PRIMARY KEY (id_address_type)
 );
 GO;
 
@@ -26,7 +34,7 @@ CREATE TABLE customer
     first_name  VARCHAR(50)        NOT NULL,
     last_name   VARCHAR(50)        NOT NULL,
     birthdate   DATE               NOT NULL,
-    deleted     BINARY             NOT NULL DEFAULT 0,
+    deleted     BIT             NOT NULL DEFAULT 0,
     PRIMARY KEY (id_customer)
 );
 GO;
@@ -37,7 +45,7 @@ CREATE TABLE staff
     first_name VARCHAR(50)        NOT NULL,
     last_name  VARCHAR(50)        NOT NULL,
     hire_date  DATE               NOT NULL,
-    deleted    BINARY             NOT NULL DEFAULT 0,
+    deleted    BIT             NOT NULL DEFAULT 0,
     id_staff_1 INT,
     id_address INT                NOT NULL,
     PRIMARY KEY (id_staff),
@@ -54,7 +62,7 @@ CREATE TABLE product
     name                   VARCHAR(50)        NOT NULL,
     vat_rate               REAL               NOT NULL,
     provisioning_threshold INT                NOT NULL,
-    deleted                BINARY             NOT NULL DEFAULT 0,
+    deleted                BIT             NOT NULL DEFAULT 0,
     quantity               INT                NOT NULL,
     buy_price              REAL               NOT NULL,
     colour                 VARCHAR(50)        NOT NULL,
@@ -69,7 +77,7 @@ CREATE TABLE [order]
     reference              VARCHAR(50)        NOT NULL,
     issue_date             VARCHAR(50)        NOT NULL,
     expected_delivery_date VARCHAR(50)        NOT NULL,
-    deleted                BINARY             NOT NULL DEFAULT 0,
+    deleted                BIT             NOT NULL DEFAULT 0,
     id_address             INT                NOT NULL,
     id_address_1           INT                NOT NULL,
     id_customer            INT                NOT NULL,
@@ -86,8 +94,8 @@ CREATE TABLE payment
     payment_date DATETIME           NOT NULL,
     payment_mean SMALLINT           NOT NULL,
     amount       REAL               NOT NULL,
-    validated    BINARY             NOT NULL DEFAULT 0,
-    deleted      BINARY             NOT NULL DEFAULT 0,
+    validated    BIT             NOT NULL DEFAULT 0,
+    deleted      BIT             NOT NULL DEFAULT 0,
     id_order     INT                NOT NULL,
     PRIMARY KEY (id_payment),
     FOREIGN KEY (id_order) REFERENCES [order] (id_order)
@@ -99,7 +107,7 @@ CREATE TABLE tiered_price
     id_tiered_price  INT IDENTITY (1,1) NOT NULL,
     minimal_quantity INT                NOT NULL,
     tf_price         REAL               NOT NULL,
-    deleted          BINARY             NOT NULL DEFAULT 0,
+    deleted          BIT             NOT NULL DEFAULT 0,
     id_product       INT                NOT NULL,
     PRIMARY KEY (id_tiered_price),
     FOREIGN KEY (id_product) REFERENCES product (id_product)
@@ -131,11 +139,12 @@ GO;
 
 CREATE TABLE customerHasAddresses
 (
-    id_address  INT,
-    id_customer INT,
-    type        INT NOT NULL,
-    PRIMARY KEY (id_address, id_customer, type),
+    id_address      INT,
+    id_customer     INT,
+    id_address_type INT,
+    PRIMARY KEY (id_address, id_customer, id_address_type),
     FOREIGN KEY (id_address) REFERENCES address (id_address),
-    FOREIGN KEY (id_customer) REFERENCES customer (id_customer)
+    FOREIGN KEY (id_customer) REFERENCES customer (id_customer),
+    FOREIGN KEY (id_address_type) REFERENCES address_type (id_address_type)
 );
 GO;
--- queries.sql
+++ queries.sql
@@ -2,20 +2,14 @@ USE [database];
 GO;
 
 /* Query to fill CustomerPage */
+/* add the count of addresseswhere deleted = 0  in the column "addressesCount" */
 SELECT c.id_customer,
        c.first_name,
        c.last_name,
-       CONVERT(VARCHAR, c.birthdate, 103)                                       AS birthdate,
-       ISNULL(b.billingAddressesCount, 0) + ISNULL(d.deliveryAddressesCount, 0) AS addressesCount
-FROM customer c
-         LEFT JOIN (SELECT id_customer, COUNT(*) AS billingAddressesCount
-                    FROM customerHasAddresses cha
-                    WHERE cha.type = 0
-                    GROUP BY id_customer) b ON c.id_customer = b.id_customer
-         LEFT JOIN (SELECT id_customer, COUNT(*) AS deliveryAddressesCount
-                    FROM customerHasAddresses cha
-                    WHERE cha.type = 1
-                    GROUP BY id_customer) d ON c.id_customer = d.id_customer; /*WHERE c.deleted = 0;*/
+       CONVERT(VARCHAR, c.birthdate, 103)                                                            AS birthdate,
+       (SELECT COUNT(*) FROM customerHasAddresses WHERE id_customer = c.id_customer AND deleted = 0) AS addressesCount
+FROM customer c/*;
+WHERE c.deleted = 0;*/
 
 /* Query to insert new customer */
 INSERT INTO customer (first_name, last_name, birthdate)
@@ -24,12 +18,14 @@ VALUES ('aa', 'bb', 'cc');
 /* Query to update a customer */
 UPDATE customer
 SET first_name = 'aa',
-    last_name = 'bb',
-    birthdate = 'cc'
+    last_name  = 'bb',
+    birthdate  = 'cc'
 WHERE id_customer = 1;
 
 /* Query to delete user (set "deleted" to 1) */
-UPDATE customer SET deleted = 1 WHERE id_customer = 1;
+UPDATE customer
+SET deleted = 1
+WHERE id_customer = 1;
 
 /* Query to fill CatalogPage */
 SELECT id_product,
@@ -44,32 +40,19 @@ SELECT id_product,
        provisioning_threshold
 FROM product;
 
-/* Query to get addresses from a client showing the type of address */
-
+/* Query to get addresses from a client */
 SELECT a.id_address,
-       a.street_name,
+       a.street,
        a.zip_code,
-       cha.type,
-       a.city
-FROM address a JOIN customerHasAddresses cha ON a.id_address = cha.id_address
-WHERE cha.id_customer = 2;
+       a.city,
+       at.type
+FROM address a
+         INNER JOIN customerHasAddresses cha ON a.id_address = cha.id_address
+         INNER JOIN address_type at ON cha.id_address_type = at.id_address_type
+WHERE cha.id_customer = 1;
 
-/* Query to create a new address and set it as a customer's address */
-INSERT INTO address (street_name, zip_code, city)
-VALUES ('aa', 28453, 'cc');
-
-INSERT INTO customerHasAddresses (id_customer, id_address, type)
-VALUES (2, 1, 0);
-
-/* Query to update an address */
-UPDATE address
-SET street_name = 'aa',
-    zip_code = 'bb',
-    city = 'cc'
-WHERE id_address = 1;
-
-INSERT INTO address (street_name, zip_code, city) VALUES ('sdf3', '44444', 'czfa')
-
-INSERT INTO customerHasAddresses (id_customer, id_address, type) VALUES ('3', '1011', '1');
-
-
+SELECT a.id_address, a.street, a.zip_code, a.city, at.type
+FROM address a
+         INNER JOIN customerHasAddresses cha ON a.id_address = cha.id_address
+         INNER JOIN address_type at ON cha.id_address_type = at.id_address_type
+WHERE cha.id_customer = 1;
